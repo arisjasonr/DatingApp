04a3280350290f3dce46309a0c34af4c24ff1689
Jason Ramirez <novarsoft@hotmail.com>
2021-03-04 21:29:44 +0800

    Add project files.

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<AppUser>(this);
+    }
+}
