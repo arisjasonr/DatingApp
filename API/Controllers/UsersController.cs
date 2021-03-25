64ec8c3d891cd0a79e6f66360caa76fe163a55fc
Jason Ramirez <novarsoft@hotmail.com>
2021-03-26 07:57:46 +0800

    End of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,24 +7,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
+        public UsersController(IUserRepository userRepository, IMapper mapper) 
+        { 
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
 
-        public UsersController(DataContext context) { _context = context; }
-
-        [AllowAnonymous]
+  
+        //public ActionResult<IEnumerable<AppUser>> GetUsers() { return _context.Users.ToList(); }
+        //public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() => Ok(await _userRepository.GetUsersAsync());
         [HttpGet()]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users =  await _userRepository.GetMembersAsync(); //await _userRepository.GetUsersAsync();
+            //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(users);
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username) 
+        {
+            //var user =  await _userRepository.GetUserByUsernameAsync(username);
+            //return _mapper.Map<MemberDto>(user);            
+            return await _userRepository.GetMemberAsync(username);          
+
+           
+        } 
+
+        //api/users/id
+
+        //[HttpGet("{username}")]
+        //public async Task<ActionResult<MemberDto>> GetUser(string username) => await _userRepository.GetUserByUsernameAsync(username);
+
+
+
+
+        //private readonly DataContext _context;
+
+
+        //public UsersController(DataContext context) { _context = context; }
+
+
+        /*[HttpGet()]
         //public ActionResult<IEnumerable<AppUser>> GetUsers() { return _context.Users.ToList(); }
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.ToListAsync(); 
 
         //api/users/id
-        [Authorize]
+        
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);
+        public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);*/
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using API.Extensions;
 
 namespace API.Entities
 {
     public class AppUser
     {
+
+
         public int Id { get; set; }
 
         public string UserName { get; set; }
@@ -14,6 +18,33 @@ namespace API.Entities
 
         public byte[] PasswordSalt { get; set; }
 
-        public override string ToString() => JsonSerializer.Serialize<AppUser>(this);
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }  
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string  City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        //public override string ToString() => JsonSerializer.Serialize<AppUser>(this);
+
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
